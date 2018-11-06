d3530fd5f4ac1f08f1ee6ae1f2e73bf5836f7928
channel960608 <997476274@qq.com>
2018-11-06 09:13:53 +0800

    update

--- weibo_crawler/weibo_crawler/pipelines.py
+++ weibo_crawler/weibo_crawler/pipelines.py
@@ -57,97 +57,107 @@ class WeiboCrawlerPipeline(object):
         return query
 
     def _conditional_insert(self, conn, item, spider):
-	    if spider.name == "weibo_fans_spider":
-	    # 判断id是否已经存在
-	    # conn.execute("""select id from user where id = "%s";"""%(item["ID"])
-	        sql = "select id from user where id = '%s'" % (item["ID"])
-	        conn.execute(sql)
-	        results = conn.fetchone()
-	        if results:
-	            conn.execute(
-                    """update user set nickname = "%s", gender = "%s", location = "%s",
-                    brief_intro = "%s", follow = "%s", followers = "%s" where id = "%s";"""%
-                    (item["nickname"],
-                    item["gender"],
-                    item["location"],
-                    item["brief_intro"],
-                    item["follow"],
-                    item["followers"],
-                    item["ID"]
-                    ))
-	        else:
-	            conn.execute(
-	                """insert into user (id, nickname, gender, location, brief_intro, follow, followers)
-                    values ("%s", "%s", "%s", "%s", "%s", "%s", "%s");"""%
-	                (item["ID"],
-	                item["nickname"],
-	                item["gender"],
-	                item["location"],
-	                item["brief_intro"],
-                    item["follow"],
-                    item["followers"]
-	                ))
-	            # conn.execute(
-	            # """insert into user (id, nickname, gender, location, brief_intro, follow, followers)
-                # values ("%s", "%s", "%s", "%s", "%s", "%s", "%s");"""%
-	            # (item["ID"],
-	            #  item["nickname"],
-	            #  item["gender"],
-	            #  item["location"],
-	            #  item["brief_intro"],
-                #  item["follow"],
-                #  item["followers"]
-	            # ))
+        try:
+            if spider.name == "user_info_spider":
+            # 判断id是否已经存在
+            # conn.execute("""select id from user where id = "%s";"""%(item["ID"])
+                
+                sql = "select id from user where id = '%s'" % (item["ID"])
+                conn.execute(sql)
+                results = conn.fetchone()
+                if results:
+                    conn.execute(
+                        """update user set nickname = "%s", gender = "%s", location = "%s",
+                        brief_intro = "%s", follow = "%s", followers = "%s" where id = "%s";"""%
+                        (item["nickname"],
+                        item["gender"],
+                        item["location"],
+                        item["brief_intro"],
+                        item["follow"],
+                        item["followers"],
+                        item["ID"]
+                        ))
+                else:
+                    conn.execute(
+                        """insert into user (id, nickname, gender, location, brief_intro, follow, followers)
+                        values ("%s", "%s", "%s", "%s", "%s", "%s", "%s");"""%
+                        (item["ID"],
+                        item["nickname"],
+                        item["gender"],
+                        item["location"],
+                        item["brief_intro"],
+                        item["follow"],
+                        item["followers"]
+                        ))
+                    # conn.execute(
+                    # """insert into user (id, nickname, gender, location, brief_intro, follow, followers)
+                    # values ("%s", "%s", "%s", "%s", "%s", "%s", "%s");"""%
+                    # (item["ID"],
+                    #  item["nickname"],
+                    #  item["gender"],
+                    #  item["location"],
+                    #  item["brief_intro"],
+                    #  item["follow"],
+                    #  item["followers"]
+                    # ))
 
-	        if "verify" in item:
-	        	conn.execute("update user set verify = '%s' where id = '%s';"%(item["verify"], item["ID"]))
-	        if "tags" in item:
-	        	conn.execute("update user set tags = '%s' where id = '%s';"%(item["tags"], item["ID"]))
-	        if "level" in item:
-	        	conn.execute("update user set level = '%s' where id = '%s';"%(item["level"], item["ID"]))
-	        if "company" in item:
-	        	conn.execute("update user set company = '%s' where id = '%s';"%(item["company"], item["ID"]))
-	        if "school" in item:
-	        	conn.execute("update user set school = '%s' where id = '%s';"%(item["school"], item["ID"]))
-	        if "blog" in item:
-	        	conn.execute("update user set blog = '%s' where id = '%s';"%(item["blog"], item["ID"]))
-	    elif spider.name == "weibo_content_spider":
-	        conn.execute(
-	        """insert into weibo_content (userid, id, pub_date, text) values ('%s', '%s', '%s', '%s');"""%
-	            (item["userid"],
-	             item["id"],
-	             item["pub_date"],
-	             item["text"]
-	            ))
-	        if "pic" in item:
-	        	conn.execute('update weibo_content set pic = "%s" where id = "%s";'%(item["pic"], item["id"]))
-	    elif spider.name == "crawler_all_users":
-	        conn.execute(
-	        """insert into fans (id, nickname, profile_image_url, profile_url, statuses_count, verified, verified_type, close_blue_v, description, gender, urank, mbtype, followers_count, follow_count, cover_image_phone, desc1, desc2) values ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s','%s');"""%
-	            (item.get("id"),
-	             item.get("nickname"),
-	             item.get("profile_image_url"),
-	             item.get("profile_url"),
-	             item.get("statuses_count"),
-	             item.get("verified"),
-	             item.get("verified_type"),
-	             item.get("close_blue_v"),
-	             item.get("description"),
-	             item.get("gender"),
-	             item.get("urank"),
-	             item.get("mbtype"),
-	             item.get("followers_count"),
-	             item.get("follow_count"),
-	             item.get("cover_image_phone"),
-	             item.get("desc1"),
-	             item.get("desc2")
-	            ))
-	    random_time = random.random() * 2
-	    time.sleep(random_time)
+                if "verify" in item:
+                    conn.execute("update user set verify = '%s' where id = '%s';"%(item["verify"], item["ID"]))
+                if "tags" in item:
+                    conn.execute("update user set tags = '%s' where id = '%s';"%(item["tags"], item["ID"]))
+                if "level" in item:
+                    conn.execute("update user set level = '%s' where id = '%s';"%(item["level"], item["ID"]))
+                if "company" in item:
+                    conn.execute("update user set company = '%s' where id = '%s';"%(item["company"], item["ID"]))
+                if "school" in item:
+                    conn.execute("update user set school = '%s' where id = '%s';"%(item["school"], item["ID"]))
+                if "blog" in item:
+                    conn.execute("update user set blog = '%s' where id = '%s';"%(item["blog"], item["ID"]))
+            elif spider.name == "weibo_content_spider":
+                conn.execute(
+                """insert into weibo_content (userid, id, pub_date, text) values ('%s', '%s', '%s', '%s');"""%
+                    (item["userid"],
+                    item["id"],
+                    item["pub_date"],
+                    item["text"]
+                    ))
+                if "pic" in item:
+                    conn.execute('update weibo_content set pic = "%s" where id = "%s";'%(item["pic"], item["id"]))
+            elif spider.name == "crawler_all_users":
+                conn.execute(
+                """insert into fans (id, nickname, profile_image_url, profile_url, statuses_count, verified, verified_type, close_blue_v, description, gender, urank, mbtype, followers_count, follow_count, cover_image_phone, desc1, desc2) values ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s','%s');"""%
+                    (item.get("id"),
+                    item.get("nickname"),
+                    item.get("profile_image_url"),
+                    item.get("profile_url"),
+                    item.get("statuses_count"),
+                    item.get("verified"),
+                    item.get("verified_type"),
+                    item.get("close_blue_v"),
+                    item.get("description"),
+                    item.get("gender"),
+                    item.get("urank"),
+                    item.get("mbtype"),
+                    item.get("followers_count"),
+                    item.get("follow_count"),
+                    item.get("cover_image_phone"),
+                    item.get("desc1"),
+                    item.get("desc2")
+                    ))
+            # random_time = random.random() * 2
+            # time.sleep(random_time)
+        except Exception as e:
+            logger.error(e)
+        finally:
+            if conn:
+                conn.close()
                 
     def handle_error(self, e):
         # log.err(e)
-        logger.error(e)
+        # logger.error(e)
+        pass
+        
+
 
 class WeiboJsonPipeline(object):
     def __init__(self):
